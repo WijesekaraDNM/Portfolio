72d1fd9f740a284b4d482f43f67a199f9f0b745f
WijesekaraDNM <masha.wijesekara@gmail.com>
2024-05-26 14:20:47 +0530

    Updating features

--- script.js
+++ script.js
@@ -107,64 +107,64 @@ function moreFunction(){
  });  
 
 //contacts
-const form = document.querySelector("contact-form");
-const fullName = document.getElementById("name");
-const mobileNumber = document.getElementById("mobileNumber");
-const email = document.getElementById("emailAddress");
-const subject = document.getElementById("emailSubject");
-const message = document.getElementById("message");
+// const form = document.querySelector("contact-form");
+// const fullName = document.getElementById("name");
+// const mobileNumber = document.getElementById("mobileNumber");
+// const email = document.getElementById("emailAddress");
+// const subject = document.getElementById("emailSubject");
+// const message = document.getElementById("message");
 
-function sendEmail(){
+// function sendEmail(){
 
-    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${emailAddress.value}
-     <br> Phone Number: ${mobileNumber.value} <br> Message: ${message.value} `;
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "99.wijesekara@gmail.com",
-        Password : "1A5A24B4D3F53A317C1F90F4FF2FD80E5B4A",
-        To : "99.wijesekara@gmail.com",
-        From : "99.wijesekara@gmail.com",
-        Subject : subject.value,
-        Body : bodyMessage
-    }).then(
-        message => {
-            if (message == "OK"){
-                Swal.fire({
-                    title: "Success!",
-                    text: "Message sent successfully!",
-                    icon: "success"
-                });
-            }
-        }
-    ).catch(
-        error => console.error(error)
-    );
-}
+//     const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${emailAddress.value}
+//      <br> Phone Number: ${mobileNumber.value} <br> Message: ${message.value} `;
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "99.wijesekara@gmail.com",
+//         Password : "1A5A24B4D3F53A317C1F90F4FF2FD80E5B4A",
+//         To : "99.wijesekara@gmail.com",
+//         From : "99.wijesekara@gmail.com",
+//         Subject : subject.value,
+//         Body : bodyMessage
+//     }).then(
+//         message => {
+//             if (message == "OK"){
+//                 Swal.fire({
+//                     title: "Success!",
+//                     text: "Message sent successfully!",
+//                     icon: "success"
+//                 });
+//             }
+//         }
+//     ).catch(
+//         error => console.error(error)
+//     );
+// }
 
-form.addEventListener("submit", (e)=> {
-    checkInputs();
-    sendEmail();
-});
+// form.addEventListener("submit", (e)=> {
+//     checkInputs();
+//     sendEmail();
+// });
 
-function checkInputs(){
-    const items = document.querySelectorAll(".item");
+// function checkInputs(){
+//     const items = document.querySelectorAll(".item");
 
-    for (const item of items ){
-        if(item.value == ""){
-            item.classList.add("error");
-            item.parentElement.classList.add("error");
+//     for (const item of items ){
+//         if(item.value == ""){
+//             item.classList.add("error");
+//             item.parentElement.classList.add("error");
 
-        }
+//         }
 
-        item.addEventListener("keyup", () =>{
-            if (item.value != ""){
-                item.classList.remove("error");
-                item.parentElement.classList.remove("error");
-            }
-            else{
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        })
-    }
-}
+//         item.addEventListener("keyup", () =>{
+//             if (item.value != ""){
+//                 item.classList.remove("error");
+//                 item.parentElement.classList.remove("error");
+//             }
+//             else{
+//                 item.classList.add("error");
+//                 item.parentElement.classList.add("error");
+//             }
+//         })
+//     }
+// }
