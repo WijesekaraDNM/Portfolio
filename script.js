4d02a1f518a0322803a1dd08500178e852ca1c2b
WijesekaraDNM <masha.wijesekara@gmail.com>
2024-06-07 14:09:48 +0530

    Changes in footer and education

--- script.js
+++ script.js
@@ -75,32 +75,6 @@ function animateIfInView(element) {
     }
 }
 
-
-// about
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const observerOptions = {
-//         root: null,
-//         rootMargin: '0px',
-//         threshold: 0.1
-//     };
-
-//     const observer = new IntersectionObserver((entries, observer) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add('in-view');
-//             } else {
-//                 entry.target.classList.remove('in-view');
-//             }
-//         });
-//     }, observerOptions);
-
-//     const aboutElements = document.querySelectorAll('.about-content h2, .about-content h3, .about-content p, .about-img img');
-//     aboutElements.forEach(element => {
-//         observer.observe(element);
-//     });
-// });
-
 function moreFunction(){
     var dots = document.getElementById("dotsId");
     var moreText = document.getElementById("more");
